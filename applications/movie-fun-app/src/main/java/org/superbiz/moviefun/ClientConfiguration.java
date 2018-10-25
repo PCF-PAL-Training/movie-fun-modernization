f2e746edaddc75d2415e7849bd99a38473fbe84e
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-10-25 09:43:31 -0600

    Securing Apps

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 import org.superbiz.moviefun.albumsapi.AlbumsClient;
 import org.superbiz.moviefun.moviesapi.MoviesClient;
 
@@ -15,17 +14,12 @@ public class ClientConfiguration {
     @Value("${movies.url}") String moviesUrl;
 
     @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
+    public AlbumsClient albumsClient(RestOperations restTemplate) {
+        return new AlbumsClient(albumsUrl, restTemplate);
     }
 
     @Bean
-    public AlbumsClient albumsClient(RestOperations restOperations) {
-        return new AlbumsClient(albumsUrl, restOperations);
-    }
-
-    @Bean
-    public MoviesClient moviesClient(RestOperations restOperations) {
-        return new MoviesClient(moviesUrl, restOperations);
+    public MoviesClient moviesClient(RestOperations restTemplate) {
+        return new MoviesClient(moviesUrl, restTemplate);
     }
 }
